3a97fd8bd916e9c5d38229d94a1f02f74b204bf1
sotirm <sotirm@umich.edu>
2020-06-02 13:35:06 -0400

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,71 @@
 # Add your code here
+
+require "pry"
+
+songs = [
+    "Phoenix - 1901",
+    "Tokyo Police Club - Wait Up",
+    "Sufjan Stevens - Too Much",
+    "The Naked and the Famous - Young Blood",
+    "(Far From) Home - Tiga",
+    "The Cults - Abducted",
+    "Phoenix - Consolation Prizes",
+    "Harry Chapin - Cats in the Cradle",
+    "Amos Lee - Keep It Loose, Keep It Tight"
+  ]
+
+
+def help
+
+    puts "I accept the following commands:"
+    puts  "- help : displays this help message"
+    puts "- list : displays a list of songs you can play"
+    puts "- play : lets you choose a song to play"
+    puts "- exit : exits this program"
+end
+
+def list(songs)
+    new_list = songs.each_with_index {|val, index| puts "#{index + 1}. #{val}"}
+    new_list
+end
+
+
+
+
+def play(songs)
+
+    puts "Please enter a song name or number:"
+    user_input = gets.strip
+    if user_input.to_i >= 1 && user_input.to_i <= songs.length
+      puts "Playing #{songs[user_input.to_i-1]}"
+    elsif songs.include?(user_input)
+      puts "Playing #{songs.find{|song| song == user_input}}"
+    else
+      puts "Invalid input, please try again"
+    end
+  end
+
+
+def exit_jukebox
+    puts "Goodbye"
+end
+
+def run(songs)
+    while true do
+      puts "Please enter a command:"
+      response = gets.chomp
+      case response
+      when "exit"
+        exit_jukebox
+        break
+      when "play"
+        play(songs)
+      when "help"
+        help
+      when "list"
+        list(songs)
+      else
+        puts "Invalid entry"
+      end
+    end
+  end
